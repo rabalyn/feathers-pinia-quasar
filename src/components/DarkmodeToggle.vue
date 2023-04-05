3e15971b575572d8b14b854c9caa35ef34562eea
Nils Mundhenke <nils.mundhenke@tu-darmstadt.de>
2023-04-05 14:45:28 +0200

    add localStorage persist

--- src/components/DarkmodeToggle.vue
+++ src/components/DarkmodeToggle.vue
@@ -14,10 +14,18 @@
 import { useQuasar } from 'quasar'
 
 const $q = useQuasar()
-
 const isDark = ref($q.dark.isActive)
+
+onMounted(() => {
+  const storedIsDark = localStorage.getItem('isDark') === 'true'
+  if (storedIsDark && storedIsDark !== isDark.value) {
+    isDark.value = !isDark.value
+    toggleDarkmode()
+  }
+})
 
 function toggleDarkmode () {
   $q.dark.toggle()
+  localStorage.setItem('isDark', isDark.value)
 }
 </script>
