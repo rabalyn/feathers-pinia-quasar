0caecbf048b5d81cecef368dea52ebe3e9305efb
Nils Mundhenke <nils.mundhenke@tu-darmstadt.de>
2023-04-05 14:44:31 +0200

    fix import from absolute path to npm link, refactor to pre.2

--- src/feathers-client.ts
+++ src/feathers-client.ts
@@ -1,5 +1,5 @@
-import { createClient } from '../../../feathers-chat-ts/src/client'
-import { createVueClient } from 'feathers-pinia'
+import { createClient } from 'feathers-chat'
+import { createPiniaClient } from 'feathers-pinia'
 import socketio from '@feathersjs/socketio-client'
 import io from 'socket.io-client'
 import { pinia } from './modules/pinia'
@@ -8,7 +8,7 @@ const host = 'http://localhost:3030'
 const socket = io(host, { transports: ['websocket'] })
 
 export const feathersClient = createClient(socketio(socket), { storage: window.localStorage })
-export const api = createVueClient(feathersClient, {
+export const api = createPiniaClient(feathersClient, {
   pinia,
   idField: 'id',
   whitelist: ['$regex'],
